29673bdef810eee979e9b24de6cf6e9faf28cc79
khosta77 <puwerfulpants@gmail.com>
2023-10-01 23:51:24 +0300

    Изменял имя инициализации SPI1. почистил от некоторых комментариев. На дынный момент тут ничего радикально важного нет. Так как просто запись пикселей

--- src/main.c
+++ src/main.c
@@ -9,22 +9,20 @@ int main(void) {
     GPIOD->MODER |= (GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0 | GPIO_MODER_MODER15_0);
     GPIOD->ODR &= ~(GPIO_ODR_OD12 | GPIO_ODR_OD13 | GPIO_ODR_OD14 | GPIO_ODR_OD15);
     MX_GPIO_Init();
-    GPIOD->ODR |= GPIO_ODR_OD12;
-    MX_SPI1_Init();
-    GPIOD->ODR |= GPIO_ODR_OD13;
+    SPI1_init();
     ST7735_Init();
-    GPIOD->ODR |= GPIO_ODR_OD14;
-	//lv_port_disp_init();
-	//lv_demo_benchmark(LV_DEMO_BENCHMARK_MODE_RENDER_AND_DRIVER);
+
     uint8_t color = 0x00;
     uint16_t colors[9] = {ST7735_BLACK, ST7735_BLUE, ST7735_RED, ST7735_GREEN, ST7735_CYAN, ST7735_MAGENTA, ST7735_YELLOW, ST7735_ORANGE, ST7735_LIGHTGREEN};
-
+    GPIOD->ODR |= GPIO_ODR_OD12;
 	while(1) {
         if (color == 9)
             color = 0x00;
         for (uint8_t y = 0; y < ST7735_HEIGHT; y++) {
             for (uint8_t x = 0; x < ST7735_WIDTH; x++) {
+                GPIOD->ODR &= ~GPIO_ODR_OD12;
                 ST7735_DrawPixel(x, y, colors[color]);
+                GPIOD->ODR |= GPIO_ODR_OD12;
                 //MyDelay(10);
                 //GPIOD->ODR ^= GPIO_ODR_OD15;
             }
