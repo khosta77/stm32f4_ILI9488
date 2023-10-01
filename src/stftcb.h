119b0a3ddb3956379f4ae016df7f232440f1263c
khosta77 <puwerfulpants@gmail.com>
2023-10-01 23:48:43 +0300

    Пока только создал хэдер. Сюда планирую перенести все функции для работы с таким дисплеями

---
+++ src/stftcb.h
@@ -0,0 +1,13 @@
+#ifndef SERIAL_TFT_CONTROL_BUS_H_
+#define SERIAL_TFT_CONTROL_BUS_H_
+
+/*
+ *  На дисплее ILI9488 и ST7735 одинаковые разъемы +- одинаковые. Это говорит о том, что подключение можно \
+ *  стандартизировать.
+ * */
+
+#include "./spi.h"
+
+
+
+#endif  // SERIAL_TFT_CONTROL_BUS_H_
