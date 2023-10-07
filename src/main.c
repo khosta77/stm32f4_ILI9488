24d1b48afc801542fcb38fe0764ecb02bcb8b954
khosta77 <puwerfulpants@gmail.com>
2023-10-07 10:32:59 +0300

    Причесал код человека осталось его разобрать

--- src/main.c
+++ src/main.c
@@ -2,16 +2,13 @@
 //#include "./spi.h"
 //#include "./st7735.h"
 
-#define ch3		(0x03<<25)
-
+#define ch3	(0x03 << 25)
 
 /*
-
-	* @brief initialize SPI1 peripheral and required pins
-  * @param  None
-  * @retval None
-
-*/
+ * @brief initialize SPI1 peripheral and required pins
+ * @param  None
+ * @retval None
+ */
 void spi1_init(void) {
 	//enable clock for GPIOA
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
@@ -38,13 +35,12 @@ void spi1_init(void) {
  * @retval None
  */
 void dma2_stream3_ch2_init(void) {
-	RCC->AHB1ENR|=RCC_AHB1ENR_DMA2EN;
-	DMA2_Stream3->CR&=~DMA_SxCR_EN;
-	while((DMA2_Stream3->CR)&DMA_SxCR_EN){;}
-	DMA2_Stream3->CR=ch3|DMA_SxCR_MINC|DMA_SxCR_DIR_0|DMA_SxCR_TCIE
-	|DMA_SxCR_HTIE|DMA_SxCR_TEIE|DMA_SxCR_DMEIE;
+	RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
+	DMA2_Stream3->CR &= ~DMA_SxCR_EN;
+	while ((DMA2_Stream3->CR) & DMA_SxCR_EN){;}
+	DMA2_Stream3->CR = (ch3 | DMA_SxCR_MINC | DMA_SxCR_DIR_0 | DMA_SxCR_TCIE | DMA_SxCR_HTIE | DMA_SxCR_TEIE | DMA_SxCR_DMEIE);
 	//DMA2_Stream3->FCR=0;
-	DMA2_Stream3->FCR&=~DMA_SxFCR_DMDIS;
+	DMA2_Stream3->FCR &= ~DMA_SxFCR_DMDIS;
 	NVIC_EnableIRQ(DMA2_Stream3_IRQn);
 }
 
@@ -55,127 +51,102 @@ void dma2_stream3_ch2_init(void) {
  * @retval None
  */
 void spi_transfer_dma(uint32_t src,uint32_t len) {
-	DMA2->LIFCR |=DMA_LIFCR_CTCIF3|DMA_LIFCR_CHTIF3|DMA_LIFCR_CTEIF3|DMA_LIFCR_CDMEIF3|DMA_LIFCR_CFEIF3;
-	DMA2_Stream3->PAR= (uint32_t)&SPI1->DR;
-	DMA2_Stream3->M0AR=src;
-	DMA2_Stream3->NDTR=len;
-	DMA2_Stream3->CR|=DMA_SxCR_EN;
+	DMA2->LIFCR |= (DMA_LIFCR_CTCIF3 | DMA_LIFCR_CHTIF3 | DMA_LIFCR_CTEIF3 | DMA_LIFCR_CDMEIF3 | DMA_LIFCR_CFEIF3);
+	DMA2_Stream3->PAR = (uint32_t)&SPI1->DR;
+	DMA2_Stream3->M0AR = src;
+	DMA2_Stream3->NDTR = len;
+	DMA2_Stream3->CR |= DMA_SxCR_EN;
 }
 
-#define	LED_red		(1U<<14)
-#define LED_green  	(1U<<12)
-#define LED_blue    (1U<<15)
-#define LED_orange  (1U<<13)
+#define	LED_red		(1U << 14)
+#define LED_green  	(1U << 12)
+#define LED_blue    (1U << 15)
+#define LED_orange  (1U << 13)
 #define	LED_PORT    GPIOD
 void led_init();
 
-#define GPIOD_CLOCK    (1<<3)
+#define GPIOD_CLOCK    (1 << 3)
 //bits of the pins
-#define LED_red_BIT    (1U<<28)
-#define LED_green_BIT  (1U<<24)
-#define LED_orange_BIT (1U<<26)
-#define LED_blue_BIT   (1U<<30)
+#define LED_red_BIT    (1U << 28)
+#define LED_green_BIT  (1U << 24)
+#define LED_orange_BIT (1U << 26)
+#define LED_blue_BIT   (1U << 30)
 //LED port
 #define	LED_PORT		GPIOD
 //bit possition for color
-#define	LED_red		 		 (1U<<14)
-#define LED_green  		 (1U<<12)
-#define LED_blue       (1U<<15)
-#define LED_orange     (1U<<13)
+#define	LED_red		   (1U << 14)
+#define LED_green  	   (1U << 12)
+#define LED_blue       (1U << 15)
+#define LED_orange     (1U << 13)
 
-	/*
-	* @brief initialize the built-in LED of STM32F407-DISCO
-  * @param  None
-  * @retval None
-
-*/
-
-void led_init()
-	{
-	RCC->AHB1ENR |=GPIOD_CLOCK;
-	GPIOD->MODER |= LED_red_BIT|LED_green_BIT|LED_orange_BIT|LED_blue_BIT;
-
-	}
-
-char data[]={'A','B','C','D','E','F','G'};
-volatile uint8_t finished=0;
-void delayMs(int delay)
-
-{
-
-volatile int i;
-
-for(; delay>0 ;delay--)
-
-{
-
-for(i =0; i<3195;i++);
-
+/*
+ * @brief initialize the built-in LED of STM32F407-DISCO
+ * @param  None
+ * @retval None
+ */
+void led_init() {
+	RCC->AHB1ENR |= GPIOD_CLOCK;
+	GPIOD->MODER |= (LED_red_BIT | LED_green_BIT | LED_orange_BIT | LED_blue_BIT);
 }
 
+char data[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+volatile uint8_t finished = 0;
+
+void delayMs(int delay) {
+    volatile int i;
+    for (; delay > 0 ;d elay--) {
+        for (i = 0; i < 3195; i++);
+    }
 }
 
-int main(void)
-	{
+int main(void) {
 	//printf("system init\r\n");
 	led_init();
 	spi1_init();
 	dma2_stream3_ch2_init();
 	//printf("system init finished\r\n");
 
-	while(1)
-
-			{
-			spi_transfer_dma((uint32_t)data,sizeof(data));
-			while(finished==0){}
-			finished=0;
-			delayMs(10);
-
-			}
+	while(1) {
+		spi_transfer_dma((uint32_t)data,sizeof(data));
+		while(finished==0){}
+		finished=0;
+		delayMs(10);
 	}
-void DMA2_Stream3_IRQHandler(void)
-		{
+}
 
-		if(DMA2->LISR&(DMA_LISR_TCIF3))
-				{
-					LED_PORT->ODR^=LED_blue;
-					//printf("finished transfered\r\n");
-					finished=1;
-					DMA2_Stream3->CR&=~DMA_SxCR_EN;
-					DMA2->LIFCR |=DMA_LIFCR_CTCIF3;
-				}
+void DMA2_Stream3_IRQHandler(void) {
+    if (DMA2->LISR & (DMA_LISR_TCIF3)) {
+		LED_PORT->ODR ^= LED_blue;
+		//printf("finished transfered\r\n");
+		finished = 1;
+		DMA2_Stream3->CR &= ~DMA_SxCR_EN;
+		DMA2->LIFCR |= DMA_LIFCR_CTCIF3;
+	}
 
-		if(DMA2->LISR&(DMA_LISR_HTIF3))
-				{
-					LED_PORT->ODR^=LED_green;
-					//printf("half transfered\r\n");
-					DMA2->LIFCR |=DMA_LIFCR_CHTIF3;
-				}
+	if (DMA2->LISR & (DMA_LISR_HTIF3)) {
+		LED_PORT->ODR ^= LED_green;
+		//printf("half transfered\r\n");
+		DMA2->LIFCR |= DMA_LIFCR_CHTIF3;
+	}
 
 
-		if(DMA2->LISR&(DMA_LISR_TEIF3))
-						{
-						//printf("transfer error interrupt\r\n");
-						DMA2->LIFCR|=(DMA_LIFCR_CTEIF3);
-						}
+    if (DMA2->LISR & (DMA_LISR_TEIF3)) {
+		//printf("transfer error interrupt\r\n");
+		DMA2->LIFCR |= (DMA_LIFCR_CTEIF3);
+	}
 
-		if(DMA2->LISR&(DMA_LISR_DMEIF3))
-						{
-					//	printf("Direct mode interrupt error\r\n");
-						DMA2->LIFCR|=(DMA_LIFCR_CDMEIF3);
-						}
+    if (DMA2->LISR & (DMA_LISR_DMEIF3)) {
+		//printf("Direct mode interrupt error\r\n");
+		DMA2->LIFCR |= (DMA_LIFCR_CDMEIF3);
+	}
+    
+    if (DMA2->LISR & (DMA_LISR_FEIF3)) {
+		//printf("FIFO error interrupt\r\n");
+		DMA2->LIFCR |= (DMA_LIFCR_CFEIF3);
+	}
 
-		if(DMA2->LISR&(DMA_LISR_FEIF3))
-						{
-					//	printf("FIFO error interrupt\r\n");
-						DMA2->LIFCR|=(DMA_LIFCR_CFEIF3);
-						}
-
-			NVIC_ClearPendingIRQ(DMA2_Stream3_IRQn);
-		}
-
-
-
+	NVIC_ClearPendingIRQ(DMA2_Stream3_IRQn);
+}
 
 #if 0
 int main(void) {
