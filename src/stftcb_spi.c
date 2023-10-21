44e62b9f36cfe5fdc37b679a128cb970c9bf18e3
khosta77 <puwerfulpants@gmail.com>
2023-10-21 23:08:13 +0300

    Сделал SPI изменяемым

--- src/stftcb_spi.c
+++ src/stftcb_spi.c
@@ -12,40 +12,46 @@ void SPI_init() {
 
 static void SPI_SPI_init() {
     // 1. Включаем тактирование
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    STFTCB_SPI_RCC;
+    //RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
     // 2. Настройка деления частоты
-    SPI1->CR1 |= ();  // (SPI_CR1_BR_1 | SPI_CR1_BR_0);
+    STFTCB_SPI->CR1 |= STFTCB_SPI_BR;  // ();  // (SPI_CR1_BR_1 | SPI_CR1_BR_0);
     // 3. Сброс битов, которые могут испортить передачу
-    SPI1->CR1 &= ~(SPI_CR1_LSBFIRST | SPI_CR1_CPHA | SPI_CR1_CPOL);
+    STFTCB_SPI->CR1 &= ~(SPI_CR1_LSBFIRST | SPI_CR1_CPHA | SPI_CR1_CPOL);
     // 3. настрока DMA
-	SPI1->CR2 |= SPI_CR2_TXDMAEN;
+	STFTCB_SPI->CR2 |= (SPI_CR2_TXDMAEN | SPI_CR2_RXDMAEN);
     // 4. Настройка CR1
     // - SPI_CR1_MSTR - мастер мод
     // - SPI_CR1_SSM - Разрешает программное управление slave устройством
     // - SPI_CR1_SSI - Надо с SPI_CR1_SSM включать. Синхронизирует что то
     // - SPI_CR1_SPE - Запуск SPI
     // - SPI_CR1_DFF - 16-битный режим передачи, его включать после инициализации дисплея
-	SPI1->CR1 |= (SPI_CR1_MSTR | /*SPI_CR1_DFF |*/ SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_SPE);
+	STFTCB_SPI->CR1 |= (SPI_CR1_MSTR | /*SPI_CR1_DFF |*/ SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_SPE);
 }
 
-/** @brief SPI1_GPIO_init - инициализация GPIO для SPI1 так как нам надо только отправлять данные в режиме \
- *                          Master, то надо инициализировать только две ножки, тактирование(SPI_SCK) и \
- *                          Master Out Slave In (SPI_MOSI).
- *                          PA5 ---> SPI1_SCK
- *                          PA7 ---> SPI1_MOSI
+/** @brief SPI1_GPIO_init - инициализация GPIO для SPI
  * */
 static void SPI_GPIO_init() {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    GPIOA->MODER |= (GPIO_MODER_MODER5_1 | GPIO_MODER_MODER7_1);
-	GPIOA->AFR[0] |= ((GPIO_AFRL_AFSEL5_2 | GPIO_AFRL_AFSEL5_0) | (GPIO_AFRL_AFSEL7_2 | GPIO_AFRL_AFSEL7_0));
+    STFTCB_SPI_GPIO_SCK_RCC;
+    STFTCB_SPI_GPIO_SCK_MODER;
+    STFTCB_SPI_GPIO_SCK_AFR;
+
+//    STFTCB_SPI_GPIO_MISO_RCC;
+//    STFTCB_SPI_GPIO_MISO_MODER;
+//    STFTCB_SPI_GPIO_MISO_AFR;
+
+    STFTCB_SPI_GPIO_MOSI_RCC;
+    STFTCB_SPI_GPIO_MOSI_MODER;
+    STFTCB_SPI_GPIO_MOSI_AFR;
 }
 
 static void SPI_DMA_init() {
-    RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
+    STFTCB_SPI_DMA_RCC;
+    //RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
 
     // Отключаем DMA, для передачи
-	DMA2_Stream3->CR &= ~DMA_SxCR_EN;
-	while ((DMA2_Stream3->CR) & DMA_SxCR_EN){;}
+	STFTCB_SPI_DMA_SxCR->CR &= ~DMA_SxCR_EN;
+	while ((STFTCB_SPI_DMA_SxCR->CR) & DMA_SxCR_EN){;}
     // Настройка контрольных регистров
     // - (0x03 << 25)     - настройка DMA канала, для SP1_TX
     // - DMA_SxCR_MINC    - Memory INCrement mode
@@ -59,32 +65,33 @@ static void SPI_DMA_init() {
     // - DMA_SxCR_HTIE    -
     // - DMA_SxCR_TEIE    -
     // - DMA_SxCR_DMEIE   -
-	DMA2_Stream3->CR = ((0x03 << 25) | DMA_SxCR_MINC | DMA_SxCR_DIR_0 | DMA_SxCR_MSIZE_0 | DMA_SxCR_PSIZE_0 | DMA_SxCR_TCIE);
+	STFTCB_SPI_DMA_SxCR->CR = (STFTCB_SPI_DMA_SxCR_CH | DMA_SxCR_MINC | DMA_SxCR_DIR_0 |
+                               DMA_SxCR_MSIZE_0 | DMA_SxCR_PSIZE_0 | DMA_SxCR_TCIE);
 	//DMA2_Stream3->FCR = 0;
 	//DMA2_Stream3->FCR &= ~DMA_SxFCR_DMDIS;
 
-    DMA2->LIFCR |= DMA_LIFCR_CTCIF3;  
-    // (DMA_LIFCR_CTCIF3 | DMA_LIFCR_CHTIF3 | DMA_LIFCR_CTEIF3 | DMA_LIFCR_CDMEIF3 | DMA_LIFCR_CFEIF3);
-	DMA2_Stream3->PAR = (uint32_t)&SPI1->DR;
-	NVIC_EnableIRQ(DMA2_Stream3_IRQn);
-    NVIC_SetPriority(DMA2_Stream3_IRQn, 2);
+    STFTCB_SPI_DMA_CTCIF;  // Включаем прерывание после успешной передачи передачи
+    //DMA2->LIFCR |= DMA_LIFCR_CTCIF3;  
+	STFTCB_SPI_DMA_SxCR->PAR = (uint32_t)&STFTCB_SPI->DR;
+	NVIC_EnableIRQ(STFTCB_SPI_DMA_IRQN);
+    NVIC_SetPriority(STFTCB_SPI_DMA_IRQN, 2);
 }
 
 void SPI_1byte_mode_on() {
-    SPI1->CR1 &= ~SPI_CR1_SPE;
-    SPI1->CR1 &= ~SPI_CR1_DFF;
-    SPI1->CR1 |= SPI_CR1_SPE;
+    STFTCB_SPI->CR1 &= ~SPI_CR1_SPE;
+    STFTCB_SPI->CR1 &= ~SPI_CR1_DFF;
+    STFTCB_SPI->CR1 |= SPI_CR1_SPE;
 }
 
 void SPI_2byte_mode_on() {
-    SPI1->CR1 &= ~SPI_CR1_SPE;
-    SPI1->CR1 |= SPI_CR1_DFF;
-    SPI1->CR1 |= SPI_CR1_SPE;
+    STFTCB_SPI->CR1 &= ~SPI_CR1_SPE;
+    STFTCB_SPI->CR1 |= SPI_CR1_DFF;
+    STFTCB_SPI->CR1 |= SPI_CR1_SPE;
 }
 
 void SPI_transmit(uint16_t dt) {
-    while(SPI1->SR & SPI_SR_BSY);  // Ждем, пока не освободится буфер передатчика
-    SPI1->DR = dt;                 // Заполняем буфер передатчика
+    while(STFTCB_SPI->SR & SPI_SR_BSY);  // Ждем, пока не освободится буфер передатчика
+    STFTCB_SPI->DR = dt;                 // Заполняем буфер передатчика
 }
 
 
